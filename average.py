f31beee71e8a4dde18745ac88c1d80b91817d1b0
Dmitry <takkand.d@gmail.com>
2019-12-24 14:51:58 +0500

    modify average.py

--- average.py
+++ average.py
@@ -18,16 +18,20 @@ import os
 
 os.system("cls")
 
-
 def main():
+    input_file = "D:\\Takkand_D\\Desktop\\out.csv"
+    output_file = 'D:\\Takkand_D\\Desktop\\result.xlsx'
     print('processing')
-    default = [
+
+    default_columns = [
         "WELL", "ZK", "ZP", "ZKA", "ZPA", "COLL", "NOB", "H", "ZONE",
         "HORIZONT"
     ]
+
     zone_name = "Объект"
 
-    df = pd.read_csv("D:\\Takkand_D\\Desktop\\out.csv", encoding='ansi')
+    # Load file
+    df = pd.read_csv(input_file, encoding='ansi')
     df = df.rename(columns={'Н_скв': 'WELL'})
     df = df[df.COLL > 0]
     df = df[df.ZONE != "-9999.99"]
@@ -36,47 +40,43 @@ def main():
 
     df['HORIZONT'] = zone_name
 
+    # Load data to sqlite
     conn = sqlite3.connect(":memory:")
     cursor = conn.cursor()
     df.to_sql("data", con=conn, if_exists='replace')
 
     params = []
-    [params.append(i) for i in list(df) if i not in default]
+    [params.append(i) for i in list(df) if i not in default_columns]
 
-    #------------------CREATE  SQL QUERY------------------#
+    #   Create 1 SQL query
     QUERY = "SELECT COLL, WELL, ZONE, round(sum(H),2) as H, "
     for param in params:
         QUERY += "round(sum(" + param + "*H)/sum(H),2) as " + param + ", "
     QUERY = QUERY[:-2]
     QUERY += " FROM data group by COLL, ZONE, WELL;"
 
-    result_list = []
-    df = pd.read_sql_query(QUERY, conn)
-    result_list.append(list(df))
-    temp = df.apply(lambda x: x.tolist(), axis=1)
+    df1 = pd.read_sql_query(QUERY, conn)
 
+    # Store query values in list
+    result_list = df.values.tolist()
+
+    # Create 2 SQL query
     QUERY_2 = "SELECT COLL, WELL, HORIZONT, round(sum(H),2) as H, "
     for param in params:
         QUERY_2 += "round(sum(" + param + "*H)/sum(H),2) as " + param + ", "
     QUERY_2 = QUERY_2[:-2]
     QUERY_2 += " FROM data group by COLL, WELL;"
 
-    df = pd.read_sql_query(QUERY_2, conn)
+    df2 = pd.read_sql_query(QUERY_2, conn)
 
     cursor.close()
     conn.close()
 
-    temp_2 = df.apply(lambda x: x.tolist(), axis=1)
+    # Concatenate result dataframes and save to excel
+    df_result = pd.concat([df1,df2])
+    df_result.to_excel(output_file)
 
-    [result_list.append(i) for i in temp]
-    [result_list.append(i) for i in temp_2]
-
-    wb = op.Workbook()
-    ws = wb.active
-    [ws.append(i) for i in result_list]
-
-    wb.save("D:\\Takkand_D\\Desktop\\result.xlsx")
-    os.system('start ""  D:\\Takkand_D\\Desktop\\result.xlsx"')
+    os.system(f'start ""  {output_file}')
     print(f"Done in {time.perf_counter()} sec")
 
 
